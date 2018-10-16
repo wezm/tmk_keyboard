a9abbaf90d49062f98211211797afdd59f4f54a4
Wesley Moore <wes@wezm.net>
2018-10-17 09:27:09 +1100

    Add my HHKB keymap

---
+++ keyboard/hhkb/keymap_wezm.c
@@ -0,0 +1,84 @@
+/* 
+ * HHKB JP Layout
+ */
+#include "keymap_common.h"
+
+// Used for SHIFT_ESC
+#define MODS_CTRL_MASK (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+
+#ifdef KEYMAP_SECTION_ENABLE
+const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
+#else
+const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
+#endif
+    /* Layer 0: Default Layer */
+    KEYMAP_JP(FN1, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, JYEN,BSPC, \
+              TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC, \
+              LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,BSLS,ENT, \
+              LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RO,  UP,  RSFT, \
+              FN0 ,ZKHK,LALT,LGUI,MHEN,     SPC,      HENK,KANA,RGUI,FN0 , LEFT,DOWN,RGHT),
+
+    /* Layer 1: HHKB mode (HHKB Fn)
+     * ,-----------------------------------------------------------.
+     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+     * |-----------------------------------------------------------|
+     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |     |
+     * |------------------------------------------------------`    |
+     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|   |    |
+     * |-----------------------------------------------------------|
+     * |       |   |   |   |   |   |  +|  -|End|PgD|Dow|   |   |   |
+     * |-----------------------------------------------------------|
+     * |   ||   |   |   |   |         |   |   |   |   ||   |   |   |
+     * `-----------------------------------------------------------'
+     */ 
+    KEYMAP_JP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
+              CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, \
+              TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,PENT, \
+              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,TRNS, \
+              TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+};
+
+
+
+/*
+ * Fn action definition
+ */
+enum function_id {
+    SHIFT_ESC,
+};
+
+#ifdef KEYMAP_SECTION_ENABLE
+const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
+#else
+const action_t fn_actions[] PROGMEM = {
+#endif
+    [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_FUNCTION(SHIFT_ESC),
+};
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    static uint8_t shift_esc_shift_mask;
+    switch (id) {
+        case SHIFT_ESC:
+            shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
+            if (record->event.pressed) {
+                if (shift_esc_shift_mask) {
+                    add_key(KC_GRV);
+                    send_keyboard_report();
+                } else {
+                    add_key(KC_ESC);
+                    send_keyboard_report();
+                }
+            } else {
+                if (shift_esc_shift_mask) {
+                    del_key(KC_GRV);
+                    send_keyboard_report();
+                } else {
+                    del_key(KC_ESC);
+                    send_keyboard_report();
+                }
+            }
+            break;
+    }
+}
